0b959a23aa312989127bd4f6507947fc391cc4bc
salman <salmanlari1001@gmail.com>
2022-09-30 16:46:02 +0530

    desied ec2 add

--- main.tf
+++ main.tf
@@ -78,7 +78,14 @@ module "sg" {
                 protocol     = "tcp"
                 cidr_blocks  = ["0.0.0.0/0"]
                 self         = null
-            }
+            },
+            # {
+            #     from_port    = "22"
+            #     to_port      = "22"
+            #     protocol     = "tcp"
+            #     cidr_blocks  = ["0.0.0.0/0"]
+            #     self         = null
+            # }
             ]
     }
     }
